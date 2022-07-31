b5d5618a2494d305cf3f5b9f5ab734d479649c6e
Li-Chih Yen <98919376+lichih-yen@users.noreply.github.com>
2022-07-30 23:53:41 -0400

    Modified userController.js setting

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -14,6 +14,38 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please add all fields");
   }
 
+  // Check if user exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+
+  res.json({ message: "Register User data" });
+
   res.json({ message: "Register User" });
 });
 
